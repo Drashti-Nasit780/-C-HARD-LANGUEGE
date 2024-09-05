a6a28b6bb0a061516a866c8c48b440cd1ee14572
Drashti Nasit <drashtinasit780@gmail.com>
2024-09-05 09:53:10 +0530

    Add files via upload

---
+++ exam 1/1.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+ {
+      int a=10,b=20,c=15,d=30;
+     clrscr();
+	  printf("Enter the value:");
+	  scanf("%d",&a);
+	  printf("Enter the value: ");
+	  scanf("%d",&b);
+	  printf("Enter the value :");
+	  scanf("%d",&c);
+	  printf("Enter the value :");
+	  scanf("%d",&d);
+
+	 switch(a=10,b=20,c=15,d=30)
+
+	 case 'a' (a+b)
+		   printf("Enter the number ",a);
+		   break;
+	 case'b'  (b-a)
+		   printf("Enter the number",b);
+		   break;
+	 case'c'  (c*d)
+		   printf("Enter the number "c);
+		   break;
+	 case 'd' (d/b)
+		   printf("Enter the number "d);
+		   break;
+
+   getch();
+
+ }
+
---
+++ exam 1/4.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+     int a=1;
+     clrscr();
+
+       printf("enter the value");
+       scanf("%d",&a);
+
+
+       for(a=1;a<10;a++)
+	   printf("enter the number",a);
+
+
+
+    getch();
+
+}
